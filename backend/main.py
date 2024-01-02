528c0ead9a61402deb106680caf7da1057b44595
Memetelve <maciek@marszalkowski.pl>
2024-01-02 21:19:05 +0100

    :sparkles:feat: add items router

--- backend/main.py
+++ backend/main.py
@@ -2,10 +2,12 @@ from fastapi import FastAPI
 
 from routes.auth import auth
 from routes.user import user
+from routes.items import items
 
 app = FastAPI()
 app.include_router(auth)
 app.include_router(user)
+app.include_router(items)
 
 if __name__ == "__main__":
     import uvicorn
