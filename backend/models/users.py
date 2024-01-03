40921d3f224a6431b9558e0eb06bf268a53b7635
Memetelve <maciek@marszalkowski.pl>
2024-01-03 18:39:41 +0100

    :sparkles:feat: add payment card model

--- backend/models/users.py
+++ backend/models/users.py
@@ -10,3 +10,10 @@ class UserRegistration(BaseModel):
 class UserLogin(BaseModel):
     username: str
     password: str
+
+
+class PaymentCard(BaseModel):
+    card_number: str
+    card_holder: str
+    expiration_date: str
+    cvv: str
