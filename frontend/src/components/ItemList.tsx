cfd923a46e6911e6c977543fc51af3dff899fd66
Memetelve <maciek@marszalkowski.pl>
2024-01-13 15:06:04 +0100

    :sparkles:feat: add animation to item card image

--- frontend/src/components/ItemList.tsx
+++ frontend/src/components/ItemList.tsx
@@ -4,24 +4,21 @@ import ItemCard from "./ItemCard";
 import constants from "@/../config.json";
 
 export default function ItemList() {
-    const [items, setItems] = useState<{ [key: string]: any }>({}); // Add type annotation for items
+    const [items, setItems] = useState<{ [key: string]: any }>({});
 
     useEffect(() => {
         fetch(`${constants.API_DOMAIN}/api/v1/items/`)
             .then((response) => response.json())
             .then((data) => {
                 setItems(data.items);
-                console.log(data);
             })
             .catch((error) => console.error("Error:", error));
     }, []);
 
     return (
-        <div className="container mx-auto">
+        <div className="container mx-auto p-20">
             <div className="flex flex-wrap -mx-4">
                 {Object.keys(items).map((key) => {
-                    console.log(items);
-                    console.log(items[key][0]);
                     return <ItemCard key={key} item={items[key]} />;
                 })}
             </div>
