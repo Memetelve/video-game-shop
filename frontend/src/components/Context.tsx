2f0dcb8099d635707154226c9ee189cc7dc571ec
Memetelve <maciek@marszalkowski.pl>
2024-01-13 15:07:32 +0100

    :sparkles:feat: add user and currency to context

--- frontend/src/components/Context.tsx
+++ frontend/src/components/Context.tsx
@@ -1,9 +1,27 @@
 import { createContext, useContext, useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
+enum Currency {
+    USD = "USD",
+    EUR = "EUR",
+    GBP = "GBP",
+    PLN = "PLN",
+}
+
+interface User {
+    id: number;
+    username: string;
+    email: string;
+    role: string;
+}
+
 const defaultContext = {
     sessionToken: "",
     setSessionToken: (token: string) => {},
+    currency: Currency.USD,
+    setCurrency: (currency: Currency) => {},
+    user: {},
+    setUser: (user: User) => {},
 };
 
 const Context = createContext(defaultContext);
@@ -18,6 +36,13 @@ export default function ContextProvider({
     children: React.ReactNode;
 }) {
     const [sessionToken, setSessionToken] = useState("");
+    const [currency, setCurrency] = useState(Currency.USD);
+    const [user, setUser] = useState<User>({
+        id: 0,
+        username: "",
+        email: "",
+        role: "",
+    });
 
     useEffect(() => {
         const sessionToken = localStorage.getItem("sessionToken") || "";
@@ -29,6 +54,10 @@ export default function ContextProvider({
             value={{
                 sessionToken: sessionToken || "",
                 setSessionToken: setSessionToken,
+                currency: currency,
+                setCurrency: setCurrency,
+                user: user,
+                setUser: setUser,
             }}
         >
             {children}
